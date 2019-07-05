a48da449ef427d050986876b3685d66981a854ca
JH <taerg89@gmail.com>
2019-07-05 20:50:34 +0900

    RxSwoft tableview  만들기

--- rxTableView/rxTableView/ViewController.swift
+++ rxTableView/rxTableView/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let indetifier: String = "TextCell"
+    let disposeBag: DisposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bindTableView()
+        
+    }
+    
+    // https://github.com/RxSwiftCommunity/RxDataSources
+    private func bindTableView() {
+        let textArray = ["1","2","3","4","5","6","7","8","9","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+        let abservable: Observable<[String]> = Observable.of(textArray)
+        
+        // 1. tableView.rx.items(cellIdentifier:String) 사용하기
+        abservable.bind(to: tableView.rx.items(cellIdentifier: indetifier)) { (index: Int, element: String, cell: UITableViewCell) in
+            cell.textLabel?.text = element
+        }.disposed(by: disposeBag)
+        
+        // 2. tableView.rx.items(cellIdentifier: String, cellType: Cell.Type) 사용하기 -> cell class 가 있는 경우 사용
+        
+        // 3. tableView.rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>) 사용하기 -> RxDataSources
     }
 
 
