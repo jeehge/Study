0ebf3ba336495e9181461aa20e77938edbf2e0c1
JH <taerg89@gmail.com>
2019-07-09 16:49:09 +0900

    tableView.rx.items(cellIdentifier: String, cellType: Cell.Type) 사용하기

--- rxTableView/rxTableView/ViewController.swift
+++ rxTableView/rxTableView/ViewController.swift
@@ -30,15 +30,23 @@ class ViewController: UIViewController {
         let abservable: Observable<[String]> = Observable.of(textArray)
         
         // 1. tableView.rx.items(cellIdentifier:String) 사용하기
-        abservable.bind(to: tableView.rx.items(cellIdentifier: indetifier)) { (index: Int, element: String, cell: UITableViewCell) in
+//        abservable.bind(to: tableView.rx.items(cellIdentifier: indetifier)) { (index: Int, element: String, cell: UITableViewCell) in
+//            cell.textLabel?.text = element
+//        }.disposed(by: disposeBag)
+        
+        // 2. tableView.rx.items(cellIdentifier: String, cellType: Cell.Type) 사용하기 -> cell class 가 있는 경우 사용
+        abservable.bind(to: tableView.rx.items(cellIdentifier: indetifier, cellType: TextCell.self)) { (index: Int, element: String, cell: TextCell) in
             cell.textLabel?.text = element
         }.disposed(by: disposeBag)
         
-        // 2. tableView.rx.items(cellIdentifier: String, cellType: Cell.Type) 사용하기 -> cell class 가 있는 경우 사용
         
         // 3. tableView.rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>) 사용하기 -> RxDataSources
     }
 
-
 }
 
+// 2 사용 체크하기 위해 만듬
+// storyboard 에서 class 지정해줘야 함
+class TextCell: UITableViewCell {
+    
+}
