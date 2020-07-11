ef64fc4ba97747a904dbb1a4bb038cda030c063c
JH <taerg89@gmail.com>
2020-07-11 21:13:54 +0900

    SwiftUI - 메모앱 만들기 Step 1. 프로젝트 생성

---
+++ SwiftUI/SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  MemoListScene.swift
+//  SwiftUIMemo
+//
+//  Created by JH on 2020/07/11.
+//  Copyright © 2020 JH. All rights reserved.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
