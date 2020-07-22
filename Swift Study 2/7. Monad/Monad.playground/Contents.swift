3c8cc920f8d4c7af6e94cd05e3091afb26b6b17c
JH <taerg89@gmail.com>
2020-07-22 23:53:20 +0900

    모나드

--- Swift Study 2/7. Monad/Monad.playground/Contents.swift
+++ Swift Study 2/7. Monad/Monad.playground/Contents.swift
@@ -29,3 +29,44 @@ let flatmappedArr: [Int] = optionalArr.flatMap { $0 }
 
 print(mappedArr)
 print(flatmappedArr)
+
+let numbers = [1, 2, 3, 4, 5]
+
+let mapped = numbers.map { $0 * 2 }
+// [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4]]
+print(mapped)
+
+//let flatMapped = numbers.flatMap { Array(repeating: $0, count: $0) }
+//// [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+//print(flatMapped)
+
+let arr = [[1, 2, 3], [4, 5, 6]]
+let optinalArr = [[1, 2, 3], [nil, 4, 5, 6]]
+
+
+let flatMapped = arr.flatMap {
+    $0
+}
+
+print(flatMapped) // [1, 2, 3, 4, 5, 6]
+
+
+let optionalFlatMapped = optinalArr.flatMap {
+    $0
+}
+
+print(optionalFlatMapped) // [Optional(1), Optional(2), Optional(3), nil, Optional(4), Optional(5), Optional(6)]
+
+let compactMapped = optinalArr.compactMap {
+    $0
+}
+
+print(compactMapped)
+
+let optinalArr2 = [[1, 2, 3], [nil, 4, 5, 6], nil]
+
+let compactMapped2 = optinalArr2.compactMap {
+    $0
+}
+
+print(compactMapped2)
