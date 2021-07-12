e1917b6e05be327d45828b28e5534a1da0817444
JH <taerg89@gmail.com>
2021-07-12 23:47:16 +0900

    ui 구성중..

--- UI-Study/Project01/Project01/ViewController/SignUp/SignUpViewController.swift
+++ UI-Study/Project01/Project01/ViewController/SignUp/SignUpViewController.swift
@@ -6,7 +6,32 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class SignUpViewController: BaseViewController {
     
+    // MARK: - UI
+    private let infoLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.numberOfLines = 2
+        $0.text = "휴대폰 번호를 입력해주세요"
+    }
+    
+    private let inputTextField: SignUpInputView = SignUpInputView()
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    private func setupView() {
+        view.addSubviews(infoLabel)
+        
+        infoLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+    }
 }
