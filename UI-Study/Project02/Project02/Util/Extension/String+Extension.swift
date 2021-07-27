4d7f4ef080047deaac334ba70bfe2ac18d6a28ec
JH <taerg89@gmail.com>
2021-07-28 00:13:44 +0900

    add util extension

--- UI-Study/Project02/Project02/Util/Extension/String+Extension.swift
+++ UI-Study/Project02/Project02/Util/Extension/String+Extension.swift
@@ -5,4 +5,15 @@
 //  Created by JH on 2021/07/27.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    func placeholder(color: UIColor) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 8
+        return NSAttributedString(string: self,
+                                  attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .regular),
+                                               .foregroundColor: color,
+                                               .paragraphStyle: paragraphStyle])
+    }
+}
